315d984763941b3e7fd14288f584dbbaa4ae3890
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-10-08 15:39:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-const prefix = '>';
-
-const token = 'NDk3ODA0MDQ2NzkxMzQ0MTM4.DpzZOA.aqzD-2G6gXPzqE2u4kP5LU0RXLI';
-
-client.login(token);
-
-
+var prefix = "?"
 
 client.on('ready', () => {
-	console.log('Hello, im ready');
-	
-	client.user.setGame('Discord', 'https://twitch.tv/kareem');
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('By Diamond Codes')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
+
+client.login(process.env.BOT_TOKEN);
